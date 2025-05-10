4761ec79a41cdc60a4d1e2e28a68216b22da1236
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-10 14:55:49 +0200

    installing dependency and folder structure for redux and auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
-  </BrowserRouter>
-)
+);
+
---
+++ frontend/src/services/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios';
+export const SERVER_URL = axios.create({
+  baseURL: 'http:localhost:5070',
+  withCredentials: true,
+});
