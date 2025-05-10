a23ce1d5ac84eda4154f6f56427ef91006ab1c6c
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-10 15:26:52 +0200

    register and login page url in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { ToastContainer } from 'react-toastify';
+import { Route, Routes } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
+import Register from './pages/Register';
+import Login from './pages/Login';
 const App = () => {
   return (
     <div>
        <ToastContainer />
-       
+       <div>
+        <Routes>
+          <Route path="/register" element={<Register/>} />
+           <Route path="/login" element={<Login/>} />
+        </Routes>
+        </div>
     </div>
   )
 }
