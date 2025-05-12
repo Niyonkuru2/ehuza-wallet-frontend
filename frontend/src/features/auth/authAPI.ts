4779ab56445e2323781f12dd76e31f417a2c0ff8
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-12 13:39:21 +0200

    getWalletBalance,depositMoney, withdrawMoney, api call

--- frontend/src/features/auth/authAPI.ts
+++ frontend/src/features/auth/authAPI.ts
@@ -1,5 +1,6 @@
 import { SERVER_URL } from '../../services/api';
 import { RegisterPayload, LoginPayload, AuthResponse, UserProfile } from '../../types/auth';
+import { WalletActionResponse, WalletBalanceResponse, WalletTransactionPayload } from '../../types/wallet';
 
 //Register user api call
 export const registerUser = async (data: RegisterPayload): Promise<AuthResponse> => {
@@ -40,6 +41,28 @@ export const getUserProfile = async (): Promise<UserProfile> => {
 export const updateUserProfile = async (data: Partial<UserProfile>): Promise<UserProfile> => {
   const response = await SERVER_URL.put('/user/update', data);
   return response.data.updatedProfile;
+};
+
+// Get wallet balance apii call
+export const getWalletBalance = async (): Promise<WalletBalanceResponse> => {
+  const response = await SERVER_URL.get('/wallet/balance');
+  return response.data;
+};
+
+// Deposit money api call
+export const depositMoney = async (
+  data: WalletTransactionPayload
+): Promise<WalletActionResponse> => {
+  const response = await SERVER_URL.post('/wallet/deposit', data);
+  return response.data;
+};
+
+// Withdraw money api call
+export const withdrawMoney = async (
+  data: WalletTransactionPayload
+): Promise<WalletActionResponse> => {
+  const response = await SERVER_URL.post('/wallet/withdraw', data);
+  return response.data;
 };
 
 
