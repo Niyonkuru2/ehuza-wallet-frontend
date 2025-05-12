dd64044c6bc7488d5cb13919cc4a4562cb766c0c
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-12 13:52:18 +0200

    separatin authapi.ts and creating walletapi.ts file

--- frontend/src/features/auth/authAPI.ts
+++ frontend/src/features/auth/authAPI.ts
@@ -1,6 +1,5 @@
 import { SERVER_URL } from '../../services/api';
 import { RegisterPayload, LoginPayload, AuthResponse, UserProfile } from '../../types/auth';
-import { TransactionHistoryResponse, WalletActionResponse, WalletBalanceResponse, WalletTransactionPayload } from '../../types/wallet';
 
 //Register user api call
 export const registerUser = async (data: RegisterPayload): Promise<AuthResponse> => {
@@ -43,34 +42,5 @@ export const updateUserProfile = async (data: Partial<UserProfile>): Promise<Use
   return response.data.updatedProfile;
 };
 
-// Get wallet balance apii call
-export const getWalletBalance = async (): Promise<WalletBalanceResponse> => {
-  const response = await SERVER_URL.get('/wallet/balance');
-  return response.data;
-};
-
-// Deposit money api call
-export const depositMoney = async (
-  data: WalletTransactionPayload
-): Promise<WalletActionResponse> => {
-  const response = await SERVER_URL.post('/wallet/deposit', data);
-  return response.data;
-};
-
-// Withdraw money api call
-export const withdrawMoney = async (
-  data: WalletTransactionPayload
-): Promise<WalletActionResponse> => {
-  const response = await SERVER_URL.post('/wallet/withdraw', data);
-  return response.data;
-};
-
-// Get latest transactions with pagination
-export const getTransactionHistory = async (page = 1, limit = 10): Promise<TransactionHistoryResponse> => {
-  const response = await SERVER_URL.get('/wallet/transactions', {
-    params: { page, limit }
-  });
-  return response.data;
-};
 
 
---
+++ frontend/src/features/wallet/walletAPI.ts
@@ -0,0 +1,33 @@
+import { SERVER_URL } from '../../services/api';
+import { TransactionHistoryResponse, WalletActionResponse, WalletBalanceResponse, WalletTransactionPayload } from '../../types/wallet';
+
+// Get wallet balance apii call
+export const getWalletBalance = async (): Promise<WalletBalanceResponse> => {
+  const response = await SERVER_URL.get('/wallet/balance');
+  return response.data;
+};
+
+// Deposit money api call
+export const depositMoney = async (
+  data: WalletTransactionPayload
+): Promise<WalletActionResponse> => {
+  const response = await SERVER_URL.post('/wallet/deposit', data);
+  return response.data;
+};
+
+// Withdraw money api call
+export const withdrawMoney = async (
+  data: WalletTransactionPayload
+): Promise<WalletActionResponse> => {
+  const response = await SERVER_URL.post('/wallet/withdraw', data);
+  return response.data;
+};
+
+// Get latest transactions with pagination
+export const getTransactionHistory = async (page = 1, limit = 10): Promise<TransactionHistoryResponse> => {
+  const response = await SERVER_URL.get('/wallet/transactions', {
+    params: { page, limit }
+  });
+  return response.data;
+};
+
