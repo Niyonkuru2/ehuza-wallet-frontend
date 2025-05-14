1a231af0e105d2d18034c413c3e19340e3ac46fa
niyokuru2 <niyonkurusam5@gmail.com>
2025-05-14 20:46:11 +0200

    real backend url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const SERVER_URL = axios.create({
-  baseURL: 'http://localhost:5070',
+  baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: false,
 });
 
